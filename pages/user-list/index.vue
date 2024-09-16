9d3338d916e21891c3d3e9c3fa0fdad15b52614e
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-09-17 00:06:21 +0700

    refactor: integration api update and delete

--- pages/user-list/index.vue
+++ pages/user-list/index.vue
@@ -9,45 +9,113 @@
             <template #body="{ data }">
               <div class="card flex justify-center">
                 <div class="flex items-center gap-2">
-                  <i class="pi pi-times-circle text-red-500 cursor-pointer" />
-                  <i class="pi pi-user-edit text-green-500 cursor-pointer" @click="handleShowDialogEdit(data)" />
+                  <i
+                    class="pi pi-times-circle text-red-500 cursor-pointer"
+                    @click="handleShowDialogDelete(data)"
+                  />
+                  <i
+                    class="pi pi-user-edit text-green-500 cursor-pointer"
+                    @click="handleShowDialogEdit(data)"
+                  />
                 </div>
               </div>
             </template>
           </Column>
         </DataTable>
-        <Dialog v-model:visible="visible" modal :header="`Edit User ${userId}`" :style="{ width: '25rem' }">
-          <span class="text-surface-500 dark:text-surface-400 block mb-8">Update your information.</span>
+        <Dialog
+          v-model:visible="visibleDialogEdit"
+          modal
+          header="Edit User"
+          :style="{ width: '25rem' }"
+        >
+          <span class="text-surface-500 dark:text-surface-400 block mb-8"
+            >Update your information.</span
+          >
           <div class="flex items-center gap-4 mb-4">
             <label for="username" class="font-semibold w-24">Username</label>
-            <InputText id="username" class="flex-auto" autocomplete="off" />
+            <InputText
+              v-model="userData.name"
+              id="username"
+              class="flex-auto"
+              autocomplete="off"
+            />
           </div>
           <div class="flex items-center gap-4 mb-2">
             <label for="email" class="font-semibold w-24">Email</label>
-            <InputText id="email" class="flex-auto" autocomplete="off" />
+            <InputText
+              v-model="userData.email"
+              id="email"
+              class="flex-auto"
+              autocomplete="off"
+            />
           </div>
-          <div class="flex items-center gap-4 mb-2">
+          <!-- <div class="flex items-center gap-4 mb-2">
             <label for="password" class="font-semibold w-24">Password</label>
-            <InputText id="password" class="flex-auto" autocomplete="off" />
-          </div>
+            <InputText
+              v-model="userData.password"
+              type="password"
+              id="password"
+              class="flex-auto"
+              autocomplete="off"
+            />
+          </div> -->
           <template #footer>
-            <Button label="Cancel" text severity="secondary" @click="visible = false" autofocus />
-            <Button label="Save" outlined severity="secondary" @click="visible = false" autofocus />
+            <Button
+              label="Cancel"
+              text
+              severity="secondary"
+              @click="visibleDialogEdit = false"
+              autofocus
+            />
+            <Button
+              label="Save"
+              outlined
+              severity="secondary"
+              @click="updateUser(userData.id)"
+              autofocus
+            />
+          </template>
+        </Dialog>
+        <Dialog
+          v-model:visible="visibleDialogDelete"
+          modal
+          header="Hapus User"
+          class="w-[400px]"
+        >
+          <p>Apakah Anda yakin ingin menghapus data ini?</p>
+          <p class="font-bold">{{ userData.id }} - {{ userData.name }}</p>
+          <template #footer>
+            <Button
+              label="Cancel"
+              text
+              severity="secondary"
+              @click="visibleDialogDelete = false"
+              autofocus
+            />
+            <Button
+              label="Save"
+              outlined
+              severity="secondary"
+              @click="deleteUser(userData.id)"
+              autofocus
+            />
           </template>
         </Dialog>
       </div>
+      <Toast />
     </div>
   </div>
 </template>
 <script setup lang="ts">
 definePageMeta({
-  layout: 'main-layout',
+  layout: "main-layout",
 });
 
 const { useIFetch } = useFetchData();
-const visible = ref(false);
+const toast = useToast();
+const visibleDialogEdit = ref(false);
+const visibleDialogDelete = ref(false);
 const userId = ref(null);
-const userData = ref(null);
 interface ResponseData {
   id: string;
   name: string;
@@ -55,24 +123,105 @@ interface ResponseData {
   password: string;
 }
 const userList = ref<ResponseData[]>([]);
+const userData = ref({
+  id: "",
+  name: "",
+  email: "",
+  password: "",
+});
 
 const handleShowDialogEdit = (data: any) => {
   userId.value = data.id;
   userData.value = data;
   if (data.id === userId.value) {
-    visible.value = true;
+    visibleDialogEdit.value = true;
+  }
+};
+
+const handleShowDialogDelete = (data: any) => {
+  userId.value = data.id;
+  userData.value = data;
+  if (data.id === userId.value) {
+    visibleDialogDelete.value = true;
   }
 };
 
 const getUserList = async () => {
   try {
-    const response = await useIFetch('/user', {
-      method: 'GET',
+    const response = await useIFetch("/user", {
+      method: "GET",
     });
     userList.value = response.data.value as ResponseData[];
   } catch (error) {
     console.error(error);
   }
+};
+
+const deleteUser = async (idUser: string) => {
+  try {
+    const response = await useIFetch(`/user/${idUser}`, { method: "DELETE" });
+    if (response.status.value === "success") {
+      toast.add({
+        severity: "success",
+        summary: "Success!",
+        detail: "Success delete user",
+        life: 3000,
+      });
+    } else {
+      toast.add({
+        severity: "error",
+        summary: "Error!",
+        detail: "Error delete user",
+        life: 3000,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    toast.add({
+      severity: "error",
+      summary: "Error!",
+      detail: "Error delete user",
+      life: 3000,
+    });
+  }
+  visibleDialogDelete.value = false;
+};
+
+const updateUser = async (idUser: string) => {
+  try {
+    const response = await useIFetch(`/user/${idUser}`, {
+      method: "PATCH",
+      body: {
+        name: userData.value.name,
+        email: userData.value.email,
+        authId: userData.value.id, // perlu makesure id darimana
+      },
+    });
+    if (response.status.value === "success") {
+      toast.add({
+        severity: "success",
+        summary: "Success!",
+        detail: "Success update user",
+        life: 3000,
+      });
+    } else {
+      toast.add({
+        severity: "error",
+        summary: "Error!",
+        detail: "Error update user",
+        life: 3000,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    toast.add({
+      severity: "error",
+      summary: "Error!",
+      detail: "Error update user",
+      life: 3000,
+    });
+  }
+  visibleDialogEdit.value = false;
 };
 
 onMounted(() => {
